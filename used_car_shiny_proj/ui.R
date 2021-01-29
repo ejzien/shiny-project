2dd290d9cee56a75642faf8969bf252513d74dd3
Ethan Zien <ejzien@gmail.com>
2021-01-29 16:51:33 -0500

    5pm updates

--- used_car_shiny_proj/ui.R
+++ used_car_shiny_proj/ui.R
@@ -9,7 +9,8 @@ shinyUI(dashboardPage(
         ),
         sidebarMenu(
             menuItem("Overall Metrics", tabName = "Tab1", icon = icon("map")),
-            menuItem("Make Based Metrics", tabName = "Tab2", icon = icon("database"))
+            menuItem("Make Based Metrics", tabName = "Tab2", icon = icon("database")),
+            menuItem("Price Range Metrics", tabName = "Tab3", icon = icon("database"))
         )
         # selectizeInput("year_sold",
         #                "Select Item to Display",
@@ -19,7 +20,7 @@ shinyUI(dashboardPage(
         tabItems(
             tabItem(tabName = "Tab1",
                     #fluidRow(fluidRow(plotOutput("year_line", height = "300px"))),
-                    fluidRow(fluidRow(plotlyOutput("year_bar", height = "300px"))),
+                    fluidRow(fluidRow(align="center",plotlyOutput("year_bar", height = "500px",width=1200))),
                     fluidRow(box(DT::dataTableOutput("raw_overall_data"),width=25))
                              #infoBoxOutput("maxBox"),
                              #infoBoxOutput("minBox"),
@@ -31,9 +32,24 @@ shinyUI(dashboardPage(
                                    makes),
                     selectizeInput("model","Model",choices=models,multiple=T,selected=c("Bronco","F150")),
                     fluidRow(fluidRow(plotOutput("milelage_box_make"))),
-                    #fluidRow(fluidRow(plotOutput("milelage_plot_make"))),
+                    #fluidRow(fluidRow(plotOutput("mileage_scatter_plot_make"))),
                     #fluidRow(fluidRow(plotOutput("milelage_box_make"))),
                     fluidRow(fluidRow(plotlyOutput("year_made_line_make"),height=25))
+                    #fluidRow(fluidRow(plotlyOutput("year_mileage_plot_make"))),
+                    #fluidRow(fluidRow(plotOutput("year_line_make"))),
+                    #fluidRow(box(DT::dataTableOutput("make_tab_table"),width=25))
+            ),
+            tabItem(tabName = "Tab3",
+                    selectizeInput("price_min",
+                                   "Min Price",
+                                   sapply(seq(0, 375000, by=25000),dollar_format())),
+                    selectizeInput("price_max",
+                                   "Max Price",
+                                   sapply(seq(0, 375000, by=25000),dollar_format()))
+                    #fluidRow(fluidRow(plotOutput("milelage_box_make"))),
+                    #fluidRow(fluidRow(plotOutput("mileage_scatter_plot_make"))),
+                    #fluidRow(fluidRow(plotOutput("milelage_box_make"))),
+                    #fluidRow(fluidRow(plotlyOutput("year_made_line_make"),height=25))
                     #fluidRow(fluidRow(plotlyOutput("year_mileage_plot_make"))),
                     #fluidRow(fluidRow(plotOutput("year_line_make"))),
                     #fluidRow(box(DT::dataTableOutput("make_tab_table"),width=25))
